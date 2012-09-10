75b6394667b82e48fbdb51180e0ee5830144f33a
asonas <hzw1258@gmail.com>
2012-09-10 12:58:19 +0900

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rognite::Application.config.secret_token = '41a8ad32105a8484993fd680d4c6e93ac0b3540c7d9937b644b162665b7c0ffb4426411467e2eb449aba46c92a4363048bddc2a38d83bf94cdde2287a6591a63'
