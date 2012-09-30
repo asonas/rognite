76ce3102fac7d6a46a1a2a7bf90e1a9b408d6037
asonas <hzw1258@gmail.com>
2012-09-30 17:57:46 +0900

    デプロイできるようにした

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,18 +7,18 @@ require 'capistrano_colors'
 require "bundler/capistrano"
 
 # RVMを利用している場合は必要
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-require 'rvm/capistrano'
-set :rvm_ruby_string, '1.9.3'
-set :rvm_type, :user
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+#require 'rvm/capistrano'
+#set :rvm_ruby_string, '1.9.3'
+#set :rvm_type, :user
 
 # リポジトリの設定
 set :application, "rognite"
-set :repository,  "git@github.com:sociareal/#{{rognite}}.git"
+set :repository,  "git@github.com:asonas/#{application}.git"
 set :scm, :git
 set :branch, "master"
 set :deploy_via, :remote_cache
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/asonas/#{application}"
 set :rails_env, "production"
 
 role :web, "rognite.asonas.jp:22"  #デプロイ先SSHポートを指定（デフォルトは22）
@@ -26,31 +26,31 @@ role :app, "rognite.asonas.jp:22"
 role :db,  "rognite.asonas.jp:22", :primary => true
 
 # SSHの設定
-set :user, "www"
+set :user, "asonas"
 ssh_options[:port] = "22"
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
-# cap deploy:setup 後、/var/www/ の権限変更
+# cap deploy:setup 権限変更
 namespace :setup do
   task :fix_permissions do
     sudo "chown -R #{user}.#{user} #{deploy_to}"
   end
 end
 after "deploy:setup", "setup:fix_permissions"
-  
+
 # Unicorn用に起動/停止タスクを変更
 namespace :deploy do
   task :start, :roles => :app do
     run "cd #{current_path}; bundle exec unicorn_rails -c config/unicorn.rb -E #{rails_env} -D"
   end
   task :restart, :roles => :app do
-    if File.exist? "/tmp/unicorn_#{application}.pid"
-      run "kill -s USR2 `cat /tmp/unicorn_#{application}.pid`"
+    if File.exist? "/home/asonas/#{application}/tmp/pids/unicorn.pid"
+      run "kill -s USR2 `cat /home/asonas/#{application}/tmp/pids/unicorn.pid`"
     end
   end
   task :stop, :roles => :app do
-    run "kill -s QUIT `cat /tmp/unicorn.pid`"
+    run "kill -s QUIT `cat /home/asonas/#{application}/tmp/pids/unicorn.pid`"
   end
 end
 
